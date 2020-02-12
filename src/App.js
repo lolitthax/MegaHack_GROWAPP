26bf8c3ba46c472eae0ac47dd07f3add563bccb4
Aline Iasmim <aline.isilvadamiao@gmail.com>
2020-02-11 21:52:07 -0300

    First commit
    Criação do header e footer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Site from './Site';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Site/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Login = props =>{
+    return(
+        <div>
+            olá mundo!
+        </div>
+    )
+}
