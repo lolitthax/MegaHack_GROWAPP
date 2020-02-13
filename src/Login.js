29cdfb372f4a163257011e28ed57e631eb9413c0
SamPinheiro <samuca132samuca@gmail.com>
2020-02-13 17:32:57 -0300

    Tela de login e cadastro, rotas e SEO

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,21 @@
 import React from 'react'
-const Login = props =>{
-    return(
+const Login = props => {
+    return (
         <div>
-            olá mundo!
+
+            <section className='contact-section' title='Imagem do formulário de login'>
+                <div className='Content-div' title='Formulário de login'>
+                    <div className='inner-width'>
+                        <img src='../Logotipo.svg' alt='logotipo da empresa' />
+                        <input type='email' className='email' placeholder='Seu e-mail' title='Campo e-mail' />
+                        <input type='password' className='password' placeholder='Senha' title='Campo senha' />
+                        <button className='Button-submit' type='submit' title='Botão para enviar'>Entrar</button>
+                        <a href='/cadastro' title='Quero me cadastrar'>Quero me cadastrar</a>
+                        <a href='#' title='Esqueci a senha'>Esqueci a senha</a>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
+export default Login
--- src/Site.js
+++ src/Site.js
@@ -1,40 +1,16 @@
-import React from 'react'
+import React from 'react';
+
 const Site = props => {
   return (
     <div>
 
-      <header className='header'>
-        <h2 className='logo' title='GrowApp'>GrowApp</h2>
-        <input type='checkbox' id='chk' />
-        <label for='chk' className='show-menu-btn'>
-          <i className='fas fa-ellipsis-h'></i>
-        </label>
-
-        <ul className='menu'>
-          <a href='#' title='Sobre'>Sobre</a>
-          <a href='#' title='Contato'>Contato</a>
-          <a href='#' title='Login'>Login</a>
-          <label for='chk' className='hide-menu-btn'>
-            <i className='fas fa-times'></i>
-          </label>
-        </ul>
-      </header>
       <main className='page-content'>
          <div className='Parallax'>
            <h1>Teste</h1>
            <p>teste de subtítulo</p>
          </div>
       </main>
-      <footer>
-        <section className='footer-container'>
-          <div className='center-col'>
-            <h2 className='logo-footer'>GrowApp</h2>
-            <hr className='border' />
-            <p className='rights-text'>© 2020 Created By Tempo 16 All Rights Reserved.</p>
-          </div>
-
-        </section>
-      </footer>
+    
     </div>
   )
 
