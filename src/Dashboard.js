a66f3015caba585447a8289f662cf9490a47bd89
lolitthax <aline.isilvadamiao@gmail.com>
2020-02-16 14:23:13 -0300

    "dashboard"

---
+++ src/Dashboard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+
+const Dashboard = props => {
+    return (
+        <div>
+            <div className='wrapper'>
+                <aside className='sidebar'>
+                    <h2>teste</h2>
+                    <ul>
+                        <li><a href='#'> <i className='fas fa-tachometer-alt'></i>Dashboard</a></li>
+                        <li><a href='#'><i className='fas fa-piggy-bank'></i>Financeiro</a></li>
+                        <li><a href='#'><i className='fas fa-users'></i>Funcionários</a></li>
+                        <li><a href='#'><i className='fas fa-box-open'></i>Estoque</a></li>
+                        <li><a href='#'> <i className='fas fa-bell'></i>Notificações</a></li>
+                        <li><a href='#'><i className='fas fa-bullhorn'></i>Marketing</a></li>
+                        <li><a href='#'><i className='fas fa-times-circle'></i>Sair</a></li>
+                    </ul>
+                    <div className='social-media'>
+                        <a href='#'><i className='fab fa-facebook-f'></i></a>
+                        <a href='#'><i className='fab fa-twitter'></i></a>
+                        <a href='#'><i className='fab fa-instagram'></i></a>
+                    </div>
+                </aside>
+                <main className='main-content'>
+                    <header className='header'>Olá novamente!</header>
+                    <div className='info'>
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed nobis ut exercitationem atque accusamus sit natus officiis totam blanditiis at eum nemo, nulla et quae eius culpa eveniet voluptatibus repellat illum tenetur, facilis porro. Quae fuga odio perferendis itaque alias sint, beatae non maiores magnam ad, veniam tenetur atque ea exercitationem earum eveniet totam ipsam magni tempora aliquid ullam possimus? Tempora nobis facere porro, praesentium magnam provident accusamus temporibus! Repellendus harum veritatis itaque molestias repudiandae ea corporis maiores non obcaecati libero, unde ipsum consequuntur aut consectetur culpa magni omnis vero odio suscipit vitae dolor quod dignissimos perferendis eos? Consequuntur!</div>
+                    </div>
+
+                </main>
+            </div>
+
+
+
+
+        </div>
+    )
+
+
+}
+export default Dashboard
